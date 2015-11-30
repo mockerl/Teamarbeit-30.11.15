39675aba81c98437593b0bf0b829c826a7beb003
mockerl <kilian.mock@hotmail.com>
2015-11-30 13:50:43 +0100

    fertiges objekt ohne kommentare

--- Teamarbeit-smo/src/smo/Zahlen.java
+++ Teamarbeit-smo/src/smo/Zahlen.java
@@ -1,5 +1,35 @@
 package smo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Zahlen {
 
+	private List<Integer> list;
+	
+	public Zahlen() {
+		list = new ArrayList<Integer>();
+	}
+	
+	public void add (int a)
+	{
+		list.add(a);
+	}
+	
+	public int min ()
+	{
+		Collections.sort(list);
+		return list.get(0);
+	}
+	
+	public int max ()
+	{
+		Collections.sort(list);
+		return list.get(list.size());
+	}
 }
