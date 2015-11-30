539d7984c144049e7192a33790e5103d7828c3e9
aoezer <aoezer@student.tgm.ac.at>
2015-11-30 13:59:53 +0100

    Die Klasse Kegel und Pyriamde erstellt

--- Teamarbeit-smo/src/smo/Rauminhalt.java
+++ Teamarbeit-smo/src/smo/Rauminhalt.java
@@ -1,5 +1,29 @@
 package smo;
 
+/**
+ * Das ist die Klasse Rauminhalt sie berechnet das Volumen von versch. Objekte
+ * 
+ * @author Özer Angelo
+ * @version 30.11.2015
+ *
+ */
 public class Rauminhalt {
+	/**
+	 * Berechnet das Volumen einer Pyramide mit quadratischer Grundfläche
+	 * 
+	 * @param s
+	 *            = die Seitenlänge Pyramide
+	 * @param h
+	 *            = die höhe der Pyramide
+	 * @return v das Volumen
+	 */
+	public double pyramide(double s, double h) {
+		double v = (s * s) * h / 3;
+		return v;
+	}
 
+	public double Kegel(double s, double h) {
+		double v = (s * s) * h / 3;
+		return v;
+	}
 }
