e757fad62ec28f475825e4a2d2e4811896d67be0
aoezer <aoezer@student.tgm.ac.at>
2015-12-02 10:54:00 +0100

    Das Programm fertig gestellt

--- Teamarbeit-smo/src/smo/Rauminhalt.java
+++ Teamarbeit-smo/src/smo/Rauminhalt.java
@@ -11,19 +11,31 @@ public class Rauminhalt {
 	/**
 	 * Berechnet das Volumen einer Pyramide mit quadratischer Grundfläche
 	 * 
-	 * @param s
-	 *            = die Seitenlänge Pyramide
-	 * @param h
-	 *            = die höhe der Pyramide
+	 * @param s ist die Seitenlänge Pyramide
+	 * @param h ist die höhe der Pyramide
 	 * @return v das Volumen
 	 */
 	public double pyramide(double s, double h) {
 		double v = (s * s) * h / 3;
 		return v;
 	}
-
-	public double Kegel(double s, double h) {
-		double v = (s * s) * h / 3;
+	/**
+	 * Berechnet das Volumen eines Kegels 
+	 * @param r ist der Radius des Kegels
+	 * @param h ist die höhe des Kegels
+	 * @return v das Volumen
+	 */
+	public double kegel(double r, double h) {
+		double v = (1/3) * (3.141*(r*r))*h;
+		return v;
+	}
+	/**
+	 * Berechnet das Volumen eines Quaders 
+	 * @param r ist der Radius des Quaders
+	 * @return v das Volumen
+	 */
+	public double quader(double r) {
+		double v = r*r*r;
 		return v;
 	}
 }
